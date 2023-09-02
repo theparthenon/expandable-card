2814b82d2b99edcc680eeb9b92157255dce85c7a
theparthenon <63166688+theparthenon@users.noreply.github.com>
2023-09-02 12:00:48 -0400

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 202,
+    sourceType: "module"
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended"
+  ],
+  rules: {
+
+  }
+};
---
+++ dist/boilerplate-card.js
@@ -0,0 +1,1 @@
+export{B as BoilerplateCard}from"./boilerplate-card-402760ac.js";import"lit/decorators";import"lit/directive";
